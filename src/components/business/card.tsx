bfc8064678187ad410861a98b426854819784d07
rtsarakaki <rtsarakaki@gmail.com>
2024-07-28 11:41:19 -0300

    feat(card component): created card component

---
+++ src/components/business/card.tsx
@@ -0,0 +1,35 @@
+import Header from '../building-blocks/text/header'
+import TextToSvgIcon from '../building-blocks/layout/text-to-svg-icon'
+import Link from 'next/link'
+
+interface ComponentProps {
+  key: number
+  title: string
+  description: string
+  icon: string
+  navigateTo: string
+}
+
+export default async function Card({
+  key,
+  title,
+  description,
+  icon,
+  navigateTo
+}: ComponentProps) {
+  return (
+    <Link
+      href={navigateTo || ''}
+      key={key}
+      className="flex flex-col w-full sm:w-6/12 md:w-4/12 lg:w-3/12"
+    >
+      <div className="border border-black rounded-sm p-5 gap-5 m-1  flex flex-col h-full">
+        <div className="flex items-center gap-2">
+          <TextToSvgIcon className="text-primary w-10 h-10" icon={icon} />
+          <Header text={title} size="1" />
+        </div>
+        <p className="flex-grow">{description}</p>
+      </div>
+    </Link>
+  )
+}
--- src/components/business/section.tsx
+++ src/components/business/section.tsx
@@ -2,6 +2,7 @@ import { getSection } from '@/graphql/section'
 import TextToSvgIcon from '../building-blocks/layout/text-to-svg-icon'
 import Header from '../building-blocks/text/header'
 import Link from 'next/link'
+import Card from './card'
 
 interface ComponentProps {
   sectionSlug: string
@@ -17,22 +18,13 @@ export default async function Section({ sectionSlug }: ComponentProps) {
             <Header text={section.title} size="3" className="mb-10 mx-1" />
             <div className="flex flex-wrap">
               {section.cards?.map((card: any, index: number) => (
-                <Link
-                  href={card?.navigateTo || ''}
+                <Card
                   key={index}
-                  className="flex flex-col w-full sm:w-6/12 md:w-4/12 lg:w-3/12"
-                >
-                  <div className="border border-black rounded-sm p-5 gap-5 m-1  flex flex-col h-full">
-                    <div className="flex items-center gap-2">
-                      <TextToSvgIcon
-                        className="text-primary w-10 h-10"
-                        icon={card.iconSvg}
-                      />
-                      <Header text={card.title} size="1" />
-                    </div>
-                    <p className="flex-grow">{card.description}</p>
-                  </div>
-                </Link>
+                  title={card.title}
+                  icon={card.iconSvg}
+                  navigateTo={card?.navigateTo || ''}
+                  description={card.description}
+                />
               ))}
             </div>
           </div>
@@ -41,7 +33,6 @@ export default async function Section({ sectionSlug }: ComponentProps) {
     }
     return (
       <>
-        <h2>Param: {sectionSlug}</h2>
         <h1>Nada a ser exibido</h1>
       </>
     )
