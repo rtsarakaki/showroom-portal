10f3069d8648f575e3ea0c50e9b1e07a7d9cb461
rtsarakaki <rtsarakaki@gmail.com>
2024-09-22 11:43:36 -0300

    ci(rollback terraform sonar fix): rollback terraform sonar fix

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,7 +1,3 @@
 resource "aws_s3_bucket" "bucket" {
 	bucket = var.s3_bucket_name
-  block_public_acls = true
-	block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
 }
