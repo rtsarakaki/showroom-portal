6f6a22bcf6d1363148835991d1c92afed38da7c0
rtsarakaki <rtsarakaki@gmail.com>
2024-09-22 11:40:46 -0300

    ci(sonar): fixing sonar hotspots

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "bucket" {
 	bucket = var.s3_bucket_name
+  block_public_acls = true
+	block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
